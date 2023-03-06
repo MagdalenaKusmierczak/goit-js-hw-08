03f94e10feb37df34b0de6eef02bde8ba92e97ef
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-03-06 19:18:01 +0100

    main construction

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,21 @@
+//Adding lodash throttle from library
+import { throttle } from 'lodash';
 
+//Initializing pre-existing player
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+// Adding key to storage
+const STORAGE_KEY = 'videoplayer-current-time';
+
+//Saving timeupdate in storage with key and tracking timeupdate with on() metod
+const onPlay = function (timeupdate) {
+  const playedTime = JSON.stringify(timeupdate);
+  localStorage.setItem(STORAGE_KEY, playedTime);
+};
+//Using lodash.throttle to update storage once for one secound
+player.on('timeupdate', throttle(onPlay, 1000));
+//Using setCurrentTime() metod to reload page and play video from saved time
+function afterReload() {}
