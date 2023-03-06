70a669e5e5be8ae691f410651f4e5bcdc49b38d5
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-03-06 20:31:07 +0100

    simplyfing

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,19 +3,20 @@ import { throttle } from 'lodash';
 
 //Initializing pre-existing player
 import Player from '@vimeo/player';
-
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 // Adding key to storage
 const STORAGE_KEY = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(STORAGE_KEY);
 
-//Saving timeupdate in storage with key and tracking timeupdate with on() metod
-const onPlay = function (timeupdate) {
-  const playedTime = JSON.stringify(timeupdate);
-  localStorage.setItem(STORAGE_KEY, playedTime);
-};
-//Using lodash.throttle to update storage once for one secound
-player.on('timeupdate', throttle(onPlay, 1000));
+//Saving timeupdate in storage with key
+function savePlayedTime(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+//Using lodash.throttle to update storage once for one secound and tracking timeupdate with on() metod
+player.on('timeupdate', throttle(savePlayedTime, 1000));
+
 //Using setCurrentTime() metod to reload page and play video from saved time
-function afterReload() {}
+player.setCurrentTime(savedTime);
